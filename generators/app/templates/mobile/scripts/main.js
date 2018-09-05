f51eb1ba9817e37137094551cb40aaf9767755f4
mrzjd <390303304@qq.com>
2018-09-05 15:40:13 +0800

    remove private code

--- generators/app/templates/mobile/scripts/main.js
+++ generators/app/templates/mobile/scripts/main.js
@@ -14,23 +14,3 @@ new Vue({
     methods: {
     }
 })
-
-/* 以下均为示例代码 - 需要则可以参考 */
-/*
-    // mobile 主播房间跳转
-    common.goRoom(config.rid);
-*/
-/*
-    // 翻页加载
-    initScroll() {
-        var ele = this.$refs.list;
-        var bh = 150;
-        var _self = this;
-        ele.addEventListener('scroll', function() {
-            var toBottomH = ele.scrollHeight - ele.scrollTop - ele.clientHeight;
-            if (toBottomH < bh) {
-                _self.getListData();
-            }
-        }, false);
-    }
-*/
